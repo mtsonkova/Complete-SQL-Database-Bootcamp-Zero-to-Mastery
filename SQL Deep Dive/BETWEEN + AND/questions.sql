1108e910e05704cb4032483ef19a08151526b53e
Milena Tsonkova <150747674+mtsonkova@users.noreply.github.com>
2024-08-15 22:51:35 +0300

    Update questions.sql
    Exercise 82 BETWEEN + AND solution

--- SQL Deep Dive/BETWEEN + AND/questions.sql
+++ SQL Deep Dive/BETWEEN + AND/questions.sql
@@ -2,10 +2,10 @@
 -- (include 30 and 50 in the results)
 
 /*
-* Write your query here
+* SELECT * FROM customers WHERE (age BETWEEN 30 AND 50) AND income < 50000
 */
 
 -- What is the average income between the ages of 20 and 50? (Including 20 and 50)
 /*
-* Write your query here
+* SELECT AVG(INCOME) from customers WHERE age BETWEEN 20 AND 50
 */
